1ddd2455c26ab59967a6c44c88109f7d8399723b
Supriya-8087 <supriya.m@prometteur.in>
2025-09-18 09:37:54 +0530

    feat: Success login

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,5 +11,7 @@ def test_login_success():
     assert token is not None, "Token should not be None"
     print(f"✅ Login successful, token received: {token}")
 
-
-
+def test_login_response_code():
+    login = LoginController()
+    response = login.login()
+    assert response.status_code == 200
